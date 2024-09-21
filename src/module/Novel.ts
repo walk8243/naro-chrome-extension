92cf5466f2bf78bbef2b9ca884ff4643e816d1e3
walk8243 <walk8243@gmail.com>
2024-09-21 23:49:30 +0900

    小説家になろうの仕様変更に対応

--- src/module/Novel.ts
+++ src/module/Novel.ts
@@ -1,43 +1,24 @@
 export class Novel {
-	static getPrevPageBtn() {
-		if(Novel.isFirstPage()) {
-			return null;
-		} else {
-			return Novel.getPaginationArea()?.getElementsByTagName('a')[0];
+	private pager?: Element;
+
+	constructor() {
+		const area = document.getElementsByClassName('p-novel')[0];
+		this.pager = area?.getElementsByClassName('c-pager')[0];
+	}
+
+	goToNextPage() {
+		if(!this.pager) { return; }
+		const next = this.pager.getElementsByClassName('c-pager__item--next')[0];
+		if(next) {
+			(next as HTMLAnchorElement).click()
 		}
 	}
 
-	static getNextPageBtn() {
-		if(Novel.isFirstPage()) {
-			return Novel.getPaginationArea()?.getElementsByTagName('a')[0];
-		} else if(Novel.isLastPage()) {
-			return null;
-		} else {
-			return Novel.getPaginationArea()?.getElementsByTagName('a')[1];
+	goToPrevPage() {
+		if(!this.pager) { return; }
+		const next = this.pager.getElementsByClassName('c-pager__item--before')[0];
+		if(next) {
+			(next as HTMLAnchorElement).click()
 		}
-	}
-
-	static isFirstPage() {
-		const page = Novel.getPageArea()?.innerText.split('/');
-		if(!page || page.length !== 2) {
-			return false;
-		}
-		return page[0] === '1';
-	}
-
-	static isLastPage() {
-		const page = Novel.getPageArea()?.innerText.split('/');
-		if(!page || page.length !== 2) {
-			return false;
-		}
-		return page[0] === page[1];
-	}
-
-	private static getPaginationArea() {
-		return document.getElementById('novel_color')?.getElementsByClassName('novel_bn')[0];
-	}
-
-	private static getPageArea() {
-		return document.getElementById('novel_no');
 	}
 }
